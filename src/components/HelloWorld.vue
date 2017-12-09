8e44a145abf7f52953ce3296ec318b798fd83af2
tgs <ddd@ss.gg>
2017-12-09 17:24:24 +0800

    Add list

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,38 @@
 <template>
+<div>
   <el-row>
-  <el-col :span="12" :offset="6">
-    <el-form ref="form" :model="form">
-      <el-form-item label="提取三元组">
-        <el-input type="textarea" v-model="form.UserInput"></el-input>
-      </el-form-item>
+    <el-col :span="12" :offset="6">
+      <el-form ref="form" :model="form">
+        <el-form-item label="提取三元组">
+          <el-input type="textarea" v-model="form.UserInput"></el-input>
+        </el-form-item>
 
-      <el-form-item>
-        <el-button class="bg" type="primary" @click="relation_triple_extraction_RULE">立即创建</el-button>
-      </el-form-item>
-    </el-form>
-  </el-col>
-</el-row>
+        <el-form-item>
+          <el-button class="bg" type="primary" @click="relation_triple_extraction_RULE">立即创建</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="12" :offset="6">
+      <ResultList />
+    </el-col>
+  </el-row>
+</div>
 </template>
 
 <script>
 /* eslint-disable */
 import axios from "axios";
+import ResultList from './ResultList'
 const ENDPOINT = "http://127.0.0.1:5000/";
-function utf16to8(str) {
-  var out, i, len, c;
-  out = "";
-  len = str.length;
-  for (i = 0; i < len; i++) {
-    c = str.charCodeAt(i);
-    if (c >= 0x0001 && c <= 0x007f) {
-      out += str.charAt(i);
-    } else if (c > 0x07ff) {
-      out += String.fromCharCode(0xe0 | ((c >> 12) & 0x0f));
-      out += String.fromCharCode(0x80 | ((c >> 6) & 0x3f));
-      out += String.fromCharCode(0x80 | ((c >> 0) & 0x3f));
-    } else {
-      out += String.fromCharCode(0xc0 | ((c >> 6) & 0x1f));
-      out += String.fromCharCode(0x80 | ((c >> 0) & 0x3f));
-    }
-  }
-  return out;
-}
+
 
 export default {
   name: "HelloWorld",
+  components: {
+    ResultList
+  },
   data() {
     return {
       form: {
