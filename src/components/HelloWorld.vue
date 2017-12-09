c595295c99b73988bbd2f6800c89a4848688a5fa
tgs <ddd@ss.gg>
2017-12-09 17:04:19 +0800

    Solve the code problem

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
       </el-form-item>
 
       <el-form-item>
-        <el-button class="bg" type="primary" @click="greet">立即创建</el-button>
+        <el-button class="bg" type="primary" @click="relation_triple_extraction_RULE">立即创建</el-button>
       </el-form-item>
     </el-form>
   </el-col>
@@ -17,6 +17,26 @@
 <script>
 /* eslint-disable */
 import axios from "axios";
+
+function utf16to8(str) {
+  var out, i, len, c;
+  out = "";
+  len = str.length;
+  for (i = 0; i < len; i++) {
+    c = str.charCodeAt(i);
+    if (c >= 0x0001 && c <= 0x007f) {
+      out += str.charAt(i);
+    } else if (c > 0x07ff) {
+      out += String.fromCharCode(0xe0 | ((c >> 12) & 0x0f));
+      out += String.fromCharCode(0x80 | ((c >> 6) & 0x3f));
+      out += String.fromCharCode(0x80 | ((c >> 0) & 0x3f));
+    } else {
+      out += String.fromCharCode(0xc0 | ((c >> 6) & 0x1f));
+      out += String.fromCharCode(0x80 | ((c >> 0) & 0x3f));
+    }
+  }
+  return out;
+}
 
 export default {
   name: "HelloWorld",
@@ -28,11 +48,10 @@ export default {
     };
   },
   methods: {
-    greet: function(event) {
-      console.log(this.form.UserInput)
+    relation_triple_extraction_RULE: function(event) {
       axios
         .post("http://127.0.0.1:5000/", {
-          idd: this.form.UserInput
+          UserInput: encodeURI(this.form.UserInput)
         })
         .then(function(response) {
           console.log(response);
@@ -40,7 +59,6 @@ export default {
         .catch(function(error) {
           console.log(error);
         });
-      console.log("Hello !");
     }
   }
 };
